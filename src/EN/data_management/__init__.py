3a1b9aeefac1cd962da6123a9df1b6cf43063fe6
Norman Metzinger <norman.metzinger@gmx.de>
2023-09-02 08:41:48 +0200

    changes

--- src/EN/data_management/__init__.py
+++ src/EN/data_management/__init__.py
@@ -2,5 +2,6 @@
 
 from EN.data_management.clean_data import clean_data
 from EN.data_management.load_data import authenticate_to_kaggle
+from EN.data_management.subset_selection import select_random_entries
 
-__all__ = [clean_data, authenticate_to_kaggle]
+__all__ = [clean_data, authenticate_to_kaggle, select_random_entries]
