13432b7ef4b21ce4ec346d4099f8eb30059336c0
Norman Metzinger <norman.metzinger@gmx.de>
2023-09-02 19:37:52 +0200

    Changes

--- src/EN/analysis/task_analysis.py
+++ src/EN/analysis/task_analysis.py
@@ -4,7 +4,7 @@ import random
 
 import pytask
 import torch
-from datasets import load_from_disk
+from datasets import Dataset, load_from_disk
 
 from EN.analysis.zero_shot import zero_shot_classifier
 from EN.config import BLD
@@ -23,11 +23,17 @@ torch.cuda.manual_seed_all(seed)
 )
 @pytask.mark.produces(BLD / "python" / "labelled" / "data_labelled_subset")
 def task_fit_model_python(depends_on, produces):
-    "Fit a logistic regression model (Python version)."
-    data = load_from_disk(depends_on["data"])
+    "Zero-shot classification that produces the labels for the data."
+    data = load_from_disk(
+        depends_on["data"],
+    )  # keep an eye of cache data being produced
     first_100_entries = data.select(range(100))
     first_100_entries = zero_shot_classifier(first_100_entries)
-    first_100_entries.save_to_disk(
-        produces,
-    )  # fix this then model is easy, just need attention and input
-    # with open(produces, "w") as json_file:
+    dataset_dict = {
+        "sequence": [item["sequence"] for item in first_100_entries],
+        "labels": [item["labels"] for item in first_100_entries],
+        "scores": [item["scores"] for item in first_100_entries],
+    }
+    dataset = Dataset.from_dict(dataset_dict)
+    dataset.save_to_disk(produces)
+    # fix this then model is easy, just need to add attention and input afterwards
