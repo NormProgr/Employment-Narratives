a37a557ba4662288521e3ba98368e7c90d1139f5
NormProgr <norman.metzinger@gmx.de>
2023-09-10 12:54:01 +0200

    problems

--- src/EN/data_management/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Functions for managing data."""
-
-from EN.data_management.clean_data import clean_data
-from EN.data_management.load_data import authenticate_to_kaggle
-
-__all__ = [clean_data, authenticate_to_kaggle]
--- src/EN/final/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Functions for formatting results."""
-
-from EN.final.cache_deletion import delete_caches_in_directory
-from EN.final.plot import table_produce
-
-__all__ = [delete_caches_in_directory, table_produce]
