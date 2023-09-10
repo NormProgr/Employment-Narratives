4cf4cf8ac7dbb575a997a881b482ed953f295233
NormProgr <norman.metzinger@gmx.de>
2023-09-10 13:05:04 +0200

    problems

--- src/EN/data_management/load_data.py
+++
@@ -1,9 +0,0 @@
-"""Loading the raw data from kaggle."""
-from kaggle.api.kaggle_api_extended import KaggleApi
-
-
-def authenticate_to_kaggle():
-    """Authenticate to Kaggle by sending the API Token."""
-    api = KaggleApi()
-    api.authenticate()
-    return api
--- src/EN/final/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Functions for formatting results."""
-
-from EN.final.cache_deletion import delete_caches_in_directory
-from EN.final.plot import plot_regression_by_age
-
-__all__ = [plot_regression_by_age, delete_caches_in_directory]
